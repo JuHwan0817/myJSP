dcba868044d0483b68dce227be398798ede59cd7
JuHwan <juhwan203@kakao.com>
2022-09-13 12:23:49 +0900

    MemberServlet

--- pro07/src/sec01/ex01/MemberDAO.java
+++ pro07/src/sec01/ex01/MemberDAO.java
@@ -1,7 +1,9 @@
 package sec01.ex01;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +20,30 @@ public class MemberDAO {
 		List<MemberVO> list = new ArrayList<MemberVO>();
 		try {
 			connDB();
+			String query = "select * from t_member";
+			System.out.println(query);
+			//SQL 문으로 회원 정보를 조회 while 문을 돌리기 위해서
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				//각 컬럼 값을 다시 MemberVO 객체의 속성에 설정
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				//설정된 MemberVO 객체를 다시 ArrayList에 저장
+				list.add(vo);
+			}
+			rs.close();
+			stmt.close();
+			con.close();
+			
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
